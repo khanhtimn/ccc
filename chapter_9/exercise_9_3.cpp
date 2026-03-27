d67d8dce2dcdcb81c43492354d84306b4fa28a63
khanhtimn <khanhlcbb@gmail.com>
2026-03-27 20:14:44 +0700

    Chapter 9: Add Exercise 9.3 - implementing and demonstrating an `all` template function.

--- chapter_9/exercise_9_3.cpp
+++ chapter_9/exercise_9_3.cpp
@@ -1,25 +1,19 @@
 #include <cstddef>
 #include <cstdio>
-#include <limits>
 
-template <typename Fn, typename In, typename Out>
-constexpr Out fold(Fn function, In* input, size_t length, Out initial) {
-  Out result{ initial };
-  for(size_t i{ 0 }; i < length; i++) {
-    result = function(result, input[i]);
+template <typename Fn, typename In>
+constexpr bool all(Fn function, In* input, size_t length) {
+  for(size_t i{ }; i < length; i++) {
+    if(!function(input[i]))
+      return false;
   }
-  return result;
+  return true;
 }
 
 int main() {
-  int data[]{ 100, 200, 300, 400, 500 };
-  const size_t data_len = 5;
-  auto sum = fold([](auto x, auto y) { return x + y; }, data, data_len, 0);
-  auto min = fold([](auto x, auto y) { return x <= y ? x : y; }, data, data_len, std::numeric_limits<int>::max());
-  auto max = fold([](auto x, auto y) { return x >= y ? x : y; }, data, data_len, std::numeric_limits<int>::min());
-  auto greater_than_200 = fold([](auto acc, auto x) { return x > 200 ? ++acc : acc; }, data, data_len, 0);
-  printf("Sum: %d\n", sum);
-  printf("Min: %d\n", min);
-  printf("Max: %d\n", max);
-  printf("Greater than 200: %d\n", greater_than_200);
+  int data[]{ 6769, 200, 300, 400, 500 };
+  size_t data_len = 5;
+  auto all_gt100 = all([](auto x) { return x > 100; }, data, data_len);
+  if(all_gt100)
+    printf("All elements greater than 100.\n");
 }
